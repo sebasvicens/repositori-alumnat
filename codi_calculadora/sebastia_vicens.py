e1ef1160f54a9118ac501d7fe42ab9e458f6d0b6
Sebastià Vicens Oliver <sebasvicensoliver99@gmail.com>
2025-04-03 10:23:42 +0200

    Informació de calculadora

---
+++ codi_calculadora/sebastia_vicens.py
@@ -0,0 +1,14 @@
+def evaluar_sebastia_vicens(operacio):
+    
+    # Escriure codi per evaluar l'expressió que ens passin dins operació
+    # variable.isnumeric() --> comprova si una variable de tipus string és numèrica o no
+    # variable.find("+") --> comprova si lo que tenim dins el find està a l'string variable (si no hi és torna -1 i si hi és torna la posició a la cual es troba)
+    # variable.split("+") --> retorna una llista amb cada una de les divisions que fa el símbol que posam a dins
+    #                         Per exemple, "3+4".split("+") --> ["3", "4"]
+    # int(variable) --> converteix la variable a tipus enter (que servirà per fer l'operació)
+
+    # Per acabar haureu de fer un return del resultat de l'operació
+
+    resultat = -1
+    
+    return resultat
